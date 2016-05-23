56774a578d3ccb17c82ccaaf29dcb48e3b16234b
Nicola Vitucci <vitucci@spaziodati.eu>
2016-05-23 13:11:06 +0200

    Added sleep in default-java-installer.sh and bumped version to 0.4.13

--- src/main/resources/embedded/default-java-installer.sh
+++ src/main/resources/embedded/default-java-installer.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 DISTRO=$(cat /etc/*-release | grep 'DISTRIB_CODENAME' | grep -oE '[a-z]+')
+WAIT_TIME=5
 echo "Our distro is $DISTRO"
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu $DISTRO main" | tee -a /etc/apt/sources.list
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu $DISTRO main" | tee -a /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+echo "Waiting for $WAIT_TIME seconds..."
+sleep $WAIT_TIME
 apt-get -q update
 echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 apt-get -qy install oracle-java7-installer
