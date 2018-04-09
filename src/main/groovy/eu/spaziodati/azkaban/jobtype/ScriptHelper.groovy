d5945aa9909012ca018e19c8620bbee647b549a2
diser55 <diser55@gmail.com>
2018-04-09 12:43:24 -0600

    Update ScriptHelper.groovy

--- src/main/groovy/eu/spaziodati/azkaban/jobtype/ScriptHelper.groovy
+++ src/main/groovy/eu/spaziodati/azkaban/jobtype/ScriptHelper.groovy
@@ -57,7 +57,7 @@ public class ScriptHelper {
             log("Trying to execute flow '$flowid' of project '$projectName' ...")
 
             def uri = new URIBuilder(endpoint)
-                .setPath(uri.getPath() + "/executor")
+            uri.setPath(uri.getPath() + "/executor")
                 .addParameter("session.id", sessionid)
                 .addParameter("ajax", "executeFlow")
                 .addParameter("project", projectName)
